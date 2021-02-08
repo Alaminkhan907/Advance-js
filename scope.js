a106fdaf3b99c4872b4539fdcf4c0bd4d7b71034
mdkhaz <mdkhaz@ttu.ee>
2021-02-08 15:51:52 +0200

    finish

---
+++ scope.js
@@ -0,0 +1,18 @@
+
+let bonus = 20;
+
+
+function sum(first,second){
+    let result = first +second +bonus;
+    //console.log(result);
+    if (result >9) {
+        var mood = 'happy';
+        console.log(mood);
+    }
+    console.log(mood);
+    var day = 'friday';
+    return result;
+}
+const output = sum(3,8);
+// console.log(output);
+// console.log(bonus);
